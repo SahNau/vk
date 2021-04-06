b2d6dd09109ad0bc5e06225f395333e85d834b53
Alexander Naumov <aleksnn00@gmail.com>
2021-04-06 17:33:00 +0500

    v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String name;
+    private String data;
+    private String text;
+    private String imageUrl;
+    private String comment;
+    private int like;
+    private int repost;
+}
