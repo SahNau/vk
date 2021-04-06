5cc83b3c6b4aca862ba84ab157ff0074ac85282d
Alexander Naumov <aleksnn00@gmail.com>
2021-04-06 19:13:19 +0500

    add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] wallSearch(int ownerId, int domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+    public void delete(int ownerID, int postID) {
+    }
+}
